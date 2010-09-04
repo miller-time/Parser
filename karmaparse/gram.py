ff4a6113de694d683c2e31968fcfbe274aa6e719
Russell <miller.time.baby@gmail.com>
2010-09-04 14:22:45 -0700

    fixed some indents

--- karmaparse/gram.py
+++ karmaparse/gram.py
@@ -23,37 +23,37 @@ def p_expression(p):
                   | down
                   """
     if len(p) > 4 and p[4] == "is":
-      result = ' '.join(p[5])
-      if p[3] not in descriptions:
-        descriptions[p[3]] = result
-      else:
-        descriptions[p[3]] += ', ' + result
-      p[0] = 'Okay.'
+        result = ' '.join(p[5])
+        if p[3] not in descriptions:
+            descriptions[p[3]] = result
+        else:
+            descriptions[p[3]] += ', ' + result
+        p[0] = 'Okay.'
     elif len(p) == 4:
-      num = things.get(p[3], 0)
-      desc = descriptions.get(p[3], '')
-      p[0] = p[3]+'('+str(num)+')'+': '+desc
+        num = things.get(p[3], 0)
+        desc = descriptions.get(p[3], '')
+        p[0] = p[3]+'('+str(num)+')'+': '+desc
     else:
-      p[0] = p[1]
+        p[0] = p[1]
 
 
 def p_up(p):
     '''up : THING PLUS PLUS'''
     if p[1] not in things:
-      things[p[1]] = 1
-      descriptions[p[1]] = ''
+        things[p[1]] = 1
+        descriptions[p[1]] = ''
     else:
-      things[p[1]] += 1
+        things[p[1]] += 1
     p[0] = 'Done'
 
 
 def p_down(p):
     '''down : THING MINUS MINUS'''
     if p[1] not in things:
-      things[p[1]] = -1
-      descriptions[p[1]] = ''
+        things[p[1]] = -1
+        descriptions[p[1]] = ''
     else:
-      things[p[1]] += -1
+        things[p[1]] += -1
     p[0] = 'Done'
 
 
